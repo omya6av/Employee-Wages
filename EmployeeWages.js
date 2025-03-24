b7e0b182bf1bca53b6da2a5fe0be199172f18f04
omya6av <omya6av@gmail.com>
2025-03-24 13:45:08 +0530

    [Om] Add. Refactor the code to write a function to get work hours.

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -6,3 +6,44 @@ if (empCheck === IS_ABSENT) {
 } else {
     console.log("Employee is PRESENT");
 }
+
+// UC-2
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+let empCheckSwitch = Math.floor(Math.random() * 10) % 3;
+
+switch (empCheckSwitch) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;
+}
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
+
+// UC-3
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+let empCheckUC3 = Math.floor(Math.random() * 10) % 3;
+let empHrsUC3 = getWorkingHours(empCheckUC3);
+let empWageUC3 = empHrsUC3 * WAGE_PER_HOUR;
+console.log("Emp Wage per working hours: " + empWageUC3);
